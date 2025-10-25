c8318995fbf9657e84e7ac4cf7ae5b520df9541e
Jonah Lee <jonahjacqueslee@gmail.com>
2025-10-24 22:26:38 -0700

    Improve product query

--- dev/product_request.py
+++ dev/product_request.py
@@ -2,27 +2,49 @@ import pprint
 import requests
 import os
 from dotenv import load_dotenv
+from pydantic import BaseModel, model_validator
+from typing import Optional
 
 load_dotenv()
 
-def query_product(token: str = None, **kwargs):
+class ProductQuery(BaseModel):
+    term: Optional[str] = None
+    productId: Optional[str] = None
+    locationId: Optional[str] = None
+    brand: Optional[str] = None
+    fulfillment: Optional[str] = None
+    start: Optional[str] = None
+    limit: Optional[str] = None
+
+    # Useful errors are already returned for invalid inputs...
+    # @model_validator(mode="after")
+    # def _has_term_or_productid(self):
+    #     """Ensure the document has one of 'term' or 'product_id' set."""
+    #     if not (self.term is not None
+    #             or self.product_id is not None):
+    #         raise ValueError("Field 'term' or 'product_id' must be used to request product information.")
+    #     return self
+
+
+def query_product(query: ProductQuery, token: str = None) -> dict:
     if token is None:
         token = os.environ.get("TOKEN")
 
-    url = "https://api.kroger.com/v1/products?"
-    for key, value in kwargs.items():
+    url: str = "https://api.kroger.com/v1/products?"
+    for key, value in query.model_dump(exclude_none=True).items():
+        print(value)
         value_formatted = value.replace(" ", "%20")
-        url += f"{key}={value_formatted}&"
+        url += f"filter.{key}={value_formatted}&"
     url = url[:-1]
 
     print(url)
 
-    headers = {
+    headers: dict = {
         'Accept': 'application/json',
         'Authorization': f'Bearer {token}'
     }
 
-    response = requests.get(url, headers=headers)
+    response: requests.Response = requests.get(url, headers=headers)
 
     if response.status_code == 200:
         return response.json()
@@ -34,11 +56,8 @@ def query_product(token: str = None, **kwargs):
 
 if __name__ == "__main__":
 
-    args = {
-        'filter.term': 'chicken soup',
-    }
-
-    products = query_product(**args)
+    query = ProductQuery(term='carrot')
+    products = query_product(query)
 
     # Print the results
     pprint.pp(products)
